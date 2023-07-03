f814b4227017b01a34095253a49d51dd1f2db75d
r0keee <106533198+r0keee@users.noreply.github.com>
2023-07-03 21:54:42 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
 const questions = [
     {
-        question: "Which is largest animal in the world?",
+        question: "When did season 6 start?",
         answers: [
-            { text: "Shark", correct: false},
-            { text: "Blue Whale", correct: true},
-            { text: "Elephant", correct: false},
-            { text: "Giraffe", correct: false},
+            { text: "20.06.22", correct: false},
+            { text: "24.06.22", correct: true},
+            { text: "24.07.22", correct: false},
+            { text: "20.06.21", correct: false},
         ]
     },
     {
-        question: "Which is the smallest continent in the world?",
+        question: "How many seasons did ReBirth have?",
         answers: [
-            { text: "Asia", correct: false},
-            { text: "Australia", correct: true},
-            { text: "Arctic", correct: false},
-            { text: "Africa", correct: false},
+            { text: "7", correct: false},
+            { text: "6", correct: false},
+            { text: "10", correct: false},
+            { text: "9", correct: true},
         ]
     },
     {
-        question: "Which is the smallest continent in the world?",
+        question: "Which of these branches did NOT exist?",
         answers: [
-            { text: "Asia", correct: false},
-            { text: "Australia", correct: true},
-            { text: "Arctic", correct: false},
-            { text: "Africa", correct: false},
+            { text: "ReBirth Terraria", correct: true},
+            { text: "ReBirth Mods", correct: false},
+            { text: "ReBirth Vanilla", correct: false},
+            { text: "ReBirth MiniGames", correct: false},
         ]
     },
     {
-        question: "Which is the smallest continent in the world?",
+        question: "What year did ReBirth appear?",
         answers: [
-            { text: "Asia", correct: false},
-            { text: "Australia", correct: true},
-            { text: "Arctic", correct: false},
-            { text: "Africa", correct: false},
+            { text: "2020", correct: true},
+            { text: "2019", correct: false},
+            { text: "2021", correct: false},
+            { text: "2022", correct: false},
+        ]
+    },
+    {
+        question: "What was the peak online at ReBirth 6?",
+        answers: [
+            { text: "7", correct: false},
+            { text: "8", correct: true},
+            { text: "9", correct: false},
+            { text: "10", correct: false},
         ]
     }
 ];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
@@ -52,6 +61,7 @@ function startQuiz(){
 }
 
 function showQuestion(){
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
@@ -60,8 +70,61 @@ function showQuestion(){
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 }
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
 
 startQuiz();
